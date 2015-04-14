7de8d90ee48ddd049d11f000ac26b85dc4c0533e
Kiril Vatev <vatev.1@gmail.com>
2015-04-13 21:54:52 -0400

    adding JSCS

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ var rs = require('replacestream');
 var path = require('path');
 
 function regexReplacer(regex, replacer) {
-	var doReplace = function(file, enc, callback) {
-		var filepath = file.path || file.history[0],
-		    name = path.basename(filepath);
+    var doReplace = function doReplace(file, enc, callback) {
+        var filepath = file.path || file.history[0];
+        var name = path.basename(filepath);
 		
         // continue if the file is null
-		if (file.isNull()) {
-			return callback(null, file);
-		}
+        if (file.isNull()) {
+            return callback(null, file);
+        }
 		
         // get the content to inject for this file
-		replacer(filepath, name, function(err, content) {
-			return doReplaceWithContent(content);
-		});
+        replacer(filepath, name, function cb(err, content) {
+            return doReplaceWithContent(content);
+        });
 
         // run a regex replace using the retrieved content
-		function doReplaceWithContent(content) {		
-			if (file.isStream()) {
-				file.contents = file.contents.pipe(rs(regex, content));
-				return callback(null, file);
-			}
+        function doReplaceWithContent(content) {
+            if (file.isStream()) {
+                file.contents = file.contents.pipe(rs(regex, content));
+                return callback(null, file);
+            }
 			
-			if (file.isBuffer()) {
-				file.contents = new Buffer(String(file.contents).replace(regex, content));
-				return callback(null, file);
-			}
-		}
-	};
+            if (file.isBuffer()) {
+                file.contents = new Buffer(String(file.contents).replace(regex, content));
+                return callback(null, file);
+            }
+        }
+    };
 	
-	return through.obj(doReplace);
+    return through.obj(doReplace);
 }
 
 module.exports = regexReplacer;
